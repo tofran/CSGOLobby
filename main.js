0a3eeedb523690af8ff227b7fe42b5e81c5894c4
Francisco Marques <kiko96kiko@gmail.com>
2015-06-18 12:25:35 +0100

    Changed to javascript loop

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
 //by Tofran
 function edit(){
+	var input = document.getElementById("textbox1").value;
 	//get the code
-	var lobby = parseInt(document.getElementById("textbox1").value.replace(" ", "").substr(22,18));
+	var lobby = parseInt(input.replace(" ", "").substr(22,18));
 
-	//change the links
-	for (i = 1; i < 10; ++i){ 
-		document.getElementById("connect" + i).href = "steam://joinlobby/730/" + lobby + i;
-	}
+	//@todo validate correcly
+	if (lobby > 0) {
+		//add the links
+		for (i = 1; i < 500; ++i){
+			//create the new link, and set the atrubutes
+			var newLink = document.createElement("a");
+			newLink.setAttribute("class", "joinLink");
+			newLink.setAttribute("target", "_blank");
+			newLink.setAttribute("href", "steam://joinlobby/730/" + lobby + i);
+			newLink.innerHTML = "Join " + i + " ";
+
+			//add the new link
+			document.getElementById('linksHere').appendChild(newLink);
+
+			if(i%8 == 0) {
+				var br = document.createElement("br");
+				document.getElementById('linksHere').appendChild(br);
+			}
+		}
+	}	
 } 
 
 
 function cleanTextBox(){
 	//clean textboxes
 	document.getElementById("textbox1").value = '';
-	//@todo clear the links
+	//clear the links
+	document.getElementById("linksHere").innerHTML = "";
 }
