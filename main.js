0c16a98699e6431a032afd546b9907d6f25fc001
Francisco Marques <kiko96kiko@gmail.com>
2015-06-18 03:00:54 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+//by Tofran
+function edit(){
+	//get the code
+	var lobby = parseInt(document.getElementById("textbox1").value;.replace(" ", "").substr(22,18));
+
+	//change the links
+	for (i = 1; i < 10; ++i){ 
+		document.getElementById("connect" + i).href = "steam://joinlobby/730/" + lobby + i;
+	}
+} 
+
+
+function cleanTextBox(){
+	//clean textboxes
+	document.getElementById("textbox1").value = '';
+	//@todo clear the links
+}
