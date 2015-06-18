2f484982b704be98f9f732c54d211702f04a2918
Francisco Marques <kiko96kiko@gmail.com>
2015-06-18 03:11:11 +0100

    -stupid bug

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //by Tofran
 function edit(){
 	//get the code
-	var lobby = parseInt(document.getElementById("textbox1").value;.replace(" ", "").substr(22,18));
+	var lobby = parseInt(document.getElementById("textbox1").value.replace(" ", "").substr(22,18));
 
 	//change the links
 	for (i = 1; i < 10; ++i){ 
